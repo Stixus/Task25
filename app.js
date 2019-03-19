d018b4f89b878455055808edc65ebbf56e40f554
Stixus <stianheimset@hotmail.com>
2019-03-19 14:16:23 +0100

    akdjawaj

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 let models = require("./models");
 
+let path = require("path");
+
 models.sequelize
   .sync()
   .then(function() {
-    console.log("Nice! Database looks fine");
+    console.log("Nice! Database looks fine.");
   })
   .catch(function(err) {
     console.log(err, "Something went wrong with the Database Update!");
@@ -26,43 +28,5 @@ app.get("*", (req, res) =>
 );
 
 module.exports = app;
-
-var path = require("path");
-
-app.get("/user/:user_id", (req, res) => {
-  res.json(data);
-});
-
-app.post("/user/create", (req, res) => {
-  res.json(data);
-});
-
-app.put("/user/update", (req, res) => {
-  res.json(data);
-});
-
-app.get("/restaurant/:restaurant_id", (req, res) => {
-  res.json(data);
-});
-
-app.put("/restaurant/update", (req, res) => {
-  res.json(data);
-});
-
-app.post("/review/create", (req, res) => {
-  res.json(data);
-});
-
-app.get("/restaurant/:restaurant_id", (req, res) => {
-  res.json(data);
-});
-
-app.get("/review/latest", (req, res) => {
-  res.json(data);
-});
-
-app.put("/review/update", (req, res) => {
-  res.json(data);
-});
 
 app.listen(process.env.PORT || 8080);
