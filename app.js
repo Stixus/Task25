dc5c827dd1171eed6c2a4189d69614e749561283
Stixus <stianheimset@hotmail.com>
2019-03-19 13:24:09 +0100

    Added many files

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
-const express = require("express");
-const companyController = require("../controllers").restaurant;
-var app = express();
+let createError = require("http-errors");
+let express = require("express");
+const bodyParser = require("body-parser");
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+let models = require("./models");
+
+models.sequelize
+  .sync()
+  .then(function() {
+    console.log("Nice! Database looks fine");
+  })
+  .catch(function(err) {
+    console.log(err, "Something went wrong with the Database Update!");
+  });
+
+require("./routes")(app);
+app.get("*", (req, res) =>
+  res.status(200).send({
+    message: "Welcome to the database."
+  })
+);
+
+module.exports = app;
 
 var path = require("path");
 
@@ -19,10 +44,6 @@ app.put("/user/update", (req, res) => {
 app.get("/restaurant/:restaurant_id", (req, res) => {
   res.json(data);
 });
-
-module.exports = app => {
-  app.post("/restaurant/create", companyController.create);
-};
 
 app.put("/restaurant/update", (req, res) => {
   res.json(data);
