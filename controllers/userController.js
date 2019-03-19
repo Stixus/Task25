94e57a6ab6a8009b3408a7e265880762b8cda53a
Stixus <stianheimset@hotmail.com>
2019-03-19 19:21:42 +0100

    Fixed get user by id

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,16 @@
 const User = require("../models").User;
 module.exports = {
+  get(req, res) {
+    return User.findOne({
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.email,
+      role: req.body.role
+    })
+      .then(user => res.status(200).send(user))
+      .catch(error => res.status(400).send(error));
+  },
+
   create(req, res) {
     return User.create({
       username: req.body.username,
@@ -10,6 +21,7 @@ module.exports = {
       .then(user => res.status(201).send(user))
       .catch(error => res.status(400).send(error));
   },
+
   update(req, res) {
     return User.update(
       {
